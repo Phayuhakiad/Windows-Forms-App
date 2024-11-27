42c5095b72cc9ac4627bf8fa124e30cc4158ab22
Phayuhakiad <65040249102@udru.ac.th>
2024-11-27 17:58:55 +0700

    Add project files.

---
+++ Windows Forms App/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Windows_Forms_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCircleArea = new System.Windows.Forms.Button();
+            this.btnTriangleArea = new System.Windows.Forms.Button();
+            this.btnHexagonArea = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtRaduis = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeitght = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtHexagonWidth = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "รัศมี";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ความสูง";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ความยาวฐาน";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ความยาวฐาน";
+            // 
+            // btnCircleArea
+            // 
+            this.btnCircleArea.Location = new System.Drawing.Point(209, 21);
+            this.btnCircleArea.Name = "btnCircleArea";
+            this.btnCircleArea.Size = new System.Drawing.Size(148, 54);
+            this.btnCircleArea.TabIndex = 4;
+            this.btnCircleArea.Text = "คำนวนพื้นที่วงกลม";
+            this.btnCircleArea.UseVisualStyleBackColor = true;
+            this.btnCircleArea.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnTriangleArea
+            // 
+            this.btnTriangleArea.Location = new System.Drawing.Point(209, 40);
+            this.btnTriangleArea.Name = "btnTriangleArea";
+            this.btnTriangleArea.Size = new System.Drawing.Size(148, 54);
+            this.btnTriangleArea.TabIndex = 5;
+            this.btnTriangleArea.Text = "คำนวนพื้นที่สามเหลี่ยม";
+            this.btnTriangleArea.UseVisualStyleBackColor = true;
+            this.btnTriangleArea.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnHexagonArea
+            // 
+            this.btnHexagonArea.Location = new System.Drawing.Point(209, 30);
+            this.btnHexagonArea.Name = "btnHexagonArea";
+            this.btnHexagonArea.Size = new System.Drawing.Size(148, 54);
+            this.btnHexagonArea.TabIndex = 6;
+            this.btnHexagonArea.Text = "คำนวนพื้นที่รูปหกเหลี่ยม";
+            this.btnHexagonArea.UseVisualStyleBackColor = true;
+            this.btnHexagonArea.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(448, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 35);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "พื้นที่ (ตารางหน่วย)";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupBox1.Controls.Add(this.txtRaduis);
+            this.groupBox1.Controls.Add(this.btnCircleArea);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(46, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(363, 91);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "คำนวนพื้นที่วงกลม";
+            // 
+            // txtRaduis
+            // 
+            this.txtRaduis.Location = new System.Drawing.Point(92, 42);
+            this.txtRaduis.Name = "txtRaduis";
+            this.txtRaduis.Size = new System.Drawing.Size(100, 22);
+            this.txtRaduis.TabIndex = 9;
+            this.txtRaduis.TextChanged += new System.EventHandler(this.txtRaduis_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.txtWidth);
+            this.groupBox2.Controls.Add(this.txtHeitght);
+            this.groupBox2.Controls.Add(this.btnTriangleArea);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(46, 143);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(363, 138);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "คำนวนพื้นที่สามเหลี่ยม";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(92, 84);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 22);
+            this.txtWidth.TabIndex = 11;
+            // 
+            // txtHeitght
+            // 
+            this.txtHeitght.Location = new System.Drawing.Point(92, 40);
+            this.txtHeitght.Name = "txtHeitght";
+            this.txtHeitght.Size = new System.Drawing.Size(100, 22);
+            this.txtHeitght.TabIndex = 10;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox3.Controls.Add(this.txtHexagonWidth);
+            this.groupBox3.Controls.Add(this.btnHexagonArea);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(46, 316);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(363, 102);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "คำนวนพื้นที่รูปหกเหลี่ยม";
+            // 
+            // txtHexagonWidth
+            // 
+            this.txtHexagonWidth.Location = new System.Drawing.Point(92, 46);
+            this.txtHexagonWidth.Name = "txtHexagonWidth";
+            this.txtHexagonWidth.Size = new System.Drawing.Size(100, 22);
+            this.txtHexagonWidth.TabIndex = 12;
+            // 
+            // lblResult
+            // 
+            this.lblResult.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblResult.ForeColor = System.Drawing.Color.Red;
+            this.lblResult.Location = new System.Drawing.Point(448, 33);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(303, 181);
+            this.lblResult.TabIndex = 11;
+            this.lblResult.Text = "...";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCircleArea;
+        private System.Windows.Forms.Button btnTriangleArea;
+        private System.Windows.Forms.Button btnHexagonArea;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtRaduis;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtHeitght;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtHexagonWidth;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Windows Forms App/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Forms_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //input  รับค่าจาก Textboxชื่อ txtRadius  (แปลงชนิดข้อมูลเป็น Double)
+            double Radius = 0;
+            //Radius = Convert.ToDouble(txtRaduis.Text);
+            if (double.TryParse(txtRaduis.Text, out Radius) == false)
+            {
+                MessageBox.Show("กรอกข้อมูลไม่ถูกต้อง", "ฮ่วย Error!!");
+                txtRaduis.Focus();    //ให้มี Cursor ที่ Textbox
+                txtRaduis.SelectAll();  //ให้ Selected ข้อความใน Textbox
+                return;
+            }
+            //process คำนวณพื้นที่ สูตร  pi*r*r
+            double CircleArea = Math.PI * Math.Pow(Radius, 2);
+            //Math.PI  ให้ค่า Pi
+            //Math.Pow(x,y)   x ยกกำลัง y
+            //output  นำไปแสดงผลที่ lblResult
+            lblResult.Text = CircleArea.ToString("0.00");
+
+            txtRaduis.Focus();
+            txtRaduis.SelectAll();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("ยินดีต้อนรับสู่โปรแกรม", "Welcome");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // ตรวจสอบค่าความสูง
+            if (double.TryParse(txtHeitght.Text, out double Height) == false)
+            {
+                MessageBox.Show("กรอกความสูงไม่ถูกต้อง", "Error");
+                txtHeitght.Focus();
+                txtHeitght.SelectAll();
+                return;
+            }
+
+            // ตรวจสอบค่าฐาน
+            if (double.TryParse(txtWidth.Text, out double Base) == false)
+            {
+                MessageBox.Show("กรอกค่าฐานไม่ถูกต้อง", "Error");
+                txtWidth.Focus();
+                txtWidth.SelectAll();
+                return;
+            }
+
+            // คำนวณพื้นที่สามเหลี่ยม (สูตร: 0.5 * ฐาน * ความสูง)
+            double TriangleArea = 0.5 * Base * Height;
+
+            // แสดงผลลัพธ์ใน lblResult
+            lblResult.Text = TriangleArea.ToString("0.00");
+
+            // ตั้งค่าโฟกัสให้กับ txtHeight
+            txtHeitght.Focus();
+            txtHeitght.SelectAll();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // ตรวจสอบค่าความยาวด้าน
+            if (double.TryParse(txtHexagonWidth.Text, out double Width) == false)
+            {
+                MessageBox.Show("กรอกค่าความยาวด้านไม่ถูกต้อง", "Error");
+                txtHexagonWidth.Focus();
+                txtHexagonWidth.SelectAll();
+                return;
+            }
+
+            // คำนวณพื้นที่รูปหกเหลี่ยมด้านเท่า
+            double HexagonArea = (3 * Math.Sqrt(3) / 2) * Math.Pow(Width, 2);
+
+            // แสดงผลลัพธ์ใน lblResult
+            lblResult.Text = HexagonArea.ToString("0.00");
+
+            // ตั้งค่าโฟกัสให้กับ txtHexagonWidth
+            txtHexagonWidth.Focus();
+            txtHexagonWidth.SelectAll();
+        }
+
+        private void txtRaduis_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("ต้องการปิดโปรแกรมใช่หรือไม่?", "Close Windows?"
+                                        , MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
